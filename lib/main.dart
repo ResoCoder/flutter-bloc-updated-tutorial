769b1be228e9d3f2ed276c5496fdff8e8769b032
Matej Resetar <mresoo99@gmail.com>
2019-06-14 08:19:49 +0200

    Updated to 0.17.0

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc/bloc.dart';
-import 'bloc/weather_bloc.dart';
 import 'model/weather.dart';
 
 void main() => runApp(MyApp());
@@ -20,50 +19,63 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WeatherPage extends StatefulWidget {
-  WeatherPage({Key key}) : super(key: key);
-
-  _WeatherPageState createState() => _WeatherPageState();
-}
-
-class _WeatherPageState extends State<WeatherPage> {
-  // Instantiate the Bloc
-  final weatherBloc = WeatherBloc();
-
+// Since version 0.17.0, you can use a stateless widget with Bloc
+class WeatherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Fake Weather App"),
       ),
+      // BlocProvider is an InheritedWidget for Blocs
       body: BlocProvider(
-        bloc: weatherBloc,
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 16),
-          alignment: Alignment.center,
-          child: BlocListener(
-            bloc: weatherBloc,
-            listener: (context, WeatherState state) {
-              if (state is WeatherLoaded) {
-                print("Loaded: ${state.weather.cityName}");
-              }
-            },
-            child: BlocBuilder(
-              bloc: weatherBloc,
-              // The builder function has to be a "pure function".
-              // That is, it only returns a Widget and doesn't do anything else.
-              builder: (BuildContext context, WeatherState state) {
-                // Changing the UI based on the current state
-                if (state is WeatherInitial) {
-                  return buildInitialInput();
-                } else if (state is WeatherLoading) {
-                  return buildLoading();
-                } else if (state is WeatherLoaded) {
-                  return buildColumnWithData(state.weather);
-                }
-              },
-            ),
-          ),
+        // This bloc can now be accessed from CityInputField
+        // It is now automatically disposed (since 0.17.0)
+        builder: (context) => WeatherBloc(),
+        child: WeatherPageChild(),
+      ),
+    );
+  }
+}
+
+// Because we now don't hold a reference to the WeatherBloc directly,
+// we have to get it through the BlocProvider. This is only possible from
+// a widget which is a child of the BlocProvider in the widget tree.
+class WeatherPageChild extends StatelessWidget {
+  const WeatherPageChild({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      alignment: Alignment.center,
+      // BlocListener invokes the listener when new state is emitted.
+      child: BlocListener(
+        bloc: BlocProvider.of<WeatherBloc>(context),
+        // Listener is the place for logging, showing Snackbars, navigating, etc.
+        // It is guaranteed to run only once per state change.
+        listener: (BuildContext context, WeatherState state) {
+          if (state is WeatherLoaded) {
+            print("Loaded: ${state.weather.cityName}");
+          }
+        },
+        // BlocBuilder invokes the builder when new state is emitted.
+        child: BlocBuilder(
+          bloc: BlocProvider.of<WeatherBloc>(context),
+          // The builder function has to be a "pure function".
+          // That is, it only returns a Widget and doesn't do anything else.
+          builder: (BuildContext context, WeatherState state) {
+            // Changing the UI based on the current state
+            if (state is WeatherInitial) {
+              return buildInitialInput();
+            } else if (state is WeatherLoading) {
+              return buildLoading();
+            } else if (state is WeatherLoaded) {
+              return buildColumnWithData(state.weather);
+            }
+          },
         ),
       ),
     );
@@ -81,6 +93,7 @@ class _WeatherPageState extends State<WeatherPage> {
     );
   }
 
+  // Builds widgets from the starter UI with custom weather data
   Column buildColumnWithData(Weather weather) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -100,12 +113,6 @@ class _WeatherPageState extends State<WeatherPage> {
         CityInputField(),
       ],
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    weatherBloc.dispose();
   }
 }
 
